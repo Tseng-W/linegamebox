bafb25935c28ac0953597a50d842ef61ba9cedc5
twen <twayne0618@gmail.com>
2019-07-07 10:38:36 +0800

    bottender init

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-const { LineBot, LineHandler, Line } = require('bottender');
-const { createServer } = require('bottender/express');
+const linebot = require('linebot');
+const express = require('express');
 
-const bot = new LineBot({
-  accessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelID: process.env.CHANNEL_ID,
-  channelSecret: process.env.CHANNEL_SECRET,
-  // sendMethod: 'reply', // Default: 'push'
+const bot = linebot({
+	channelId: process.env.CHANNEL_ID,
+	channelSecret: process.env.CHANNEL_SECRET,
+	channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 });
 
-bot.onEvent(async context => {
-  await context.sendText('Hello World');
+const app = express();
+
+const linebotParser = bot.parser();
+
+app.get('/',function(req,res){
+    res.send('Hello World!');
 });
 
-const server = createServer(bot);
+app.post('/linewebhook', linebotParser);
 
-server.listen((process.env.PORT || 5000), () => {
-  console.log('server is running on 5000 port...');
+bot.on('message', function (event) {
+	event.reply(event.message.text).then(function (data) {
+		console.log('Success', data);
+	}).catch(function (error) {
+		console.log('Error', error);
+	});
+});
+
+app.listen(process.env.PORT || 80, function () {
+	console.log('LineBot is running.');
 });
