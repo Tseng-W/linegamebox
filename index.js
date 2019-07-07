a4a2b3fb072ea809439da0c45ba70daafe6e8c2c
twen <twayne0618@gmail.com>
2019-07-07 09:59:44 +0800

    bottender init

---
+++ index.js
@@ -0,0 +1,21 @@
+const { LineBot } = require('bottender');
+const { createServer } = require('bottender/express');
+
+const config = require('./bottender.config.js').line;
+
+const bot = new LineBot({
+  accessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelID: process.env.CHANNEL_ID;
+  channelSecret: process.env.CHANNEL_SECRET,
+  sendMethod: 'reply', // Default: 'push'
+});
+
+bot.onEvent(async context => {
+  await context.sendText('Hello World');
+});
+
+const server = createServer(bot);
+
+server.listen((process.env.PORT || 5000), () => {
+  console.log('server is running on 5000 port...');
+});
