f0eb64ede7742d24ce14b7a8b69682c39041d797
twen <twayne0618@gmail.com>
2019-07-08 22:24:22 +0800

    add multi dice

--- index.js
+++ index.js
@@ -189,7 +189,7 @@ app.listen(process.env.PORT || 80, function() {
 });
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max+1));
+    return Math.floor(Math.random() * max) + 1;
 }
 
 function getRandomInt(max, amount) {
