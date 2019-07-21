234843e9c7a6df1536da8f070d43434c770dec89
twen <twayne0618@gmail.com>
2019-07-21 16:51:12 +0800

    add sql

--- index.js
+++ index.js
@@ -55,9 +55,10 @@ bot.on('message', function(event) {
                             id = profile.userId;
                             name = profile.displayName;
                             result = getUserDataFromDatabase(id);
-                            console.log("Output result at event : " + getUserDataFromDatabase(id));
+                            return result;
+                            // console.log("Output result at event : " + getUserDataFromDatabase(id));
                         })
-                        .then(function() {
+                        .then(function(result) {
                             console.log("******RESULT******* = "+result);
                             if(result === undefined){
                                 var str = getRandomInt(10);
