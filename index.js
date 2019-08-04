9fa0f733e4befc81d276367f643e8a57e4712b4f
twen <twayne0618@gmail.com>
2019-08-04 22:05:02 +0800

    add draw

--- index.js
+++ index.js
@@ -160,6 +160,7 @@ bot.on('message', function(event) {
                     //     });
                     //     break;
                 case '測風向':
+                    let drawTimes = 10;
                     for (let draw = 0; draw < drawTimes; draw++) {
                         let drawResult = Math.random() * 100;
                         console.log("Draw:"+draw+" = "+drawResult);
