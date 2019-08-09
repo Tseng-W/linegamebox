52cc4c729aea5d27d5b59c893c3733c73939fee8
twen <twayne0618@gmail.com>
2019-08-09 22:59:25 +0800

    add pgp

--- db.js
+++ db.js
@@ -2,10 +2,14 @@ const initOptions = {
     promiseLib: Promise
 };
 
-const pgPromise = require('pg-promise')(initOptions);
-const pg = 'postgres://yfqwtlcqklltsf:123fcb85f17cc4233729ea77e0fa69e5a8048e5269a6c3af49e576867c59be5d@ec2-174-129-227-205.compute-1.amazonaws.com:5432/dai9s7ljceh6ei';
-const db = pgPromise(pg);
+const pgp = require('pg-promise')(initOptions);
+const connectionString = 'postgres://yfqwtlcqklltsf:123fcb85f17cc4233729ea77e0fa69e5a8048e5269a6c3af49e576867c59be5d@ec2-174-129-227-205.compute-1.amazonaws.com:5432/dai9s7ljceh6ei';
+const db = pgp(connectionString);
 
 console.log('connection ok');
 
 module.export = db;
+
+db.on('error', function(err){
+    console.error("connection error;", err);
+});
