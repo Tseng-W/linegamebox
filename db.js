331c590aaa1847d23adb24341a4ff9747e6ffe39
twen <twayne0618@gmail.com>
2019-08-09 23:07:37 +0800

    add pgp

--- db.js
+++ db.js
@@ -3,13 +3,15 @@ const initOptions = {
 };
 
 const pgp = require('pg-promise')(initOptions);
-const connectionString = 'postgres://yfqwtlcqklltsf:123fcb85f17cc4233729ea77e0fa69e5a8048e5269a6c3af49e576867c59be5d@ec2-174-129-227-205.compute-1.amazonaws.com:5432/dai9s7ljceh6ei';
+//const connectionString = 'postgres://yfqwtlcqklltsf:123fcb85f17cc4233729ea77e0fa69e5a8048e5269a6c3af49e576867c59be5d@ec2-174-129-227-205.compute-1.amazonaws.com:5432/dai9s7ljceh6ei';
+const connectionString = process.env.DATABASE_URL;
 const db = pgp(connectionString);
 
+console.log('process.env.DATABASE_URL = '+process.env.DATABASE_URL);
 console.log('connection ok');
 
 module.export = db;
 
-db.on('error', function(err){
-    console.error("connection error;", err);
-});
+// db.on('error', function(err){
+//     console.error("connection error;", err);
+// });
