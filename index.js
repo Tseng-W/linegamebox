51595680f47096f9888969bfe5897c7227c36c6a
twen <twayne0618@gmail.com>
2019-08-09 22:55:00 +0800

    add pgp

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const linebot = require('linebot');
 const express = require('express');
-const db = require('./db.js');
+const db = require('../db.js');
 const fgoDrawProperty = [0.7,0.3,4,3,12,40,40];
 const fgoDrawResultText = ["PU五星從者","非PU五星從者","五星禮裝","四星從者","四星禮裝","三星禮裝","三星從者"];
 
