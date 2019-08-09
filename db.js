bece8d06e9015ff8a7540f54b82d18a567d60153
twen <twayne0618@gmail.com>
2019-08-09 22:53:54 +0800

    add pgp

--- db.js
+++ db.js
@@ -6,6 +6,6 @@ const pgPromise = require('pg-promise')(initOptions);
 const pg = 'postgres://yfqwtlcqklltsf:123fcb85f17cc4233729ea77e0fa69e5a8048e5269a6c3af49e576867c59be5d@ec2-174-129-227-205.compute-1.amazonaws.com:5432/dai9s7ljceh6ei';
 const db = pgPromise(pg);
 
-module.export = {
-    pgPromise, db
-};
+console.log('connection ok');
+
+module.export = db;
