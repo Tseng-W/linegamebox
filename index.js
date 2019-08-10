07031cf267f462ccf08d08b52f787ad734832521
twen <twayne0618@gmail.com>
2019-08-10 12:42:15 +0800

    split fgo util

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ bot.on('message', function(event) {
                     break;
                     case 'PGP':
                         var herosFromDB = await fgoUtil.getHerosByStar(5);
-                        console.log(herosFromDB);
+                        console.log("index.js  ------ herosFromDB : "herosFromDB);
                         // for(let index = 0;index<herosFromDB.length;index++)
                         //     console.log(herosFromDB[index]);
                     break;
