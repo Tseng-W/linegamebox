e6fa416bbd1a3c42cf2d9e15a04ab300e1f770e6
twen <twayne0618@gmail.com>
2019-08-24 08:11:17 +0800

    fix

--- fgoUtil.js
+++ fgoUtil.js
@@ -287,7 +287,7 @@ function setUserData(id, drawTimes, drawResult, callback) {
                 userData.servantPu5 += drawResult[0];
                 userData.servant5 += drawResult[1];
                 let currentLuk = getLucky(userData.drawTimes, userData.servantPu5, userData.servant5);
-                let resultText = "\n累積抽卡" + userData.drawTimes "次並歐出PU5星" + userData.servantPu5 + "位、歪出常駐5星" + userData.servant5 + "位！\n";
+                let resultText = "\n累積抽卡" + userData.drawTimes + "次並歐出PU5星" + userData.servantPu5 + "位、歪出常駐5星" + userData.servant5 + "位！\n";
                 if (originalLuk - currentLuk > 0)
                     resultText += "\n歐度從" + originalLuk + "增加到" + currentLuk + "！\n";
                 else resultText += "\n歐度從" + originalLuk + "下降到" + currentLuk + "！\n";
